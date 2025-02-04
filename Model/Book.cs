526a7fc4ad4dc95b948c5b49b8aa9879325aaf3e
Артур Стариков <Gibyte.mail@gmail.com>
2025-02-04 17:30:03 +0300

    add  Book

---
+++ Model/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentLibrary.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public bool IsAvailable { get; set; } = true;
+    }
+}
