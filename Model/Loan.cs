c37414a096c82a17fa402d74e3920062ae646e71
Артур Стариков <Gibyte.mail@gmail.com>
2025-02-04 17:38:16 +0300

    Прокоментировал Loan

--- Model/Loan.cs
+++ Model/Loan.cs
@@ -1,13 +1,15 @@
-﻿namespace StudentLibrary.Model
+﻿using static System.Reflection.Metadata.BlobBuilder;
+
+namespace StudentLibrary.Model
 {
     public class Loan
     {
-        public int Id { get; set; }
-        public int BookId { get; set; }
-        public Book Book { get; set; }
-        public int StudentId { get; set; }
-        public Student Student { get; set; }
-        public DateTime LoanDate { get; set; } = DateTime.Now;
-        public DateTime? ReturnDate { get; set; }
+        public int Id { get; set; } // Уникальный идентификатор выдачи книги. Каждая запись в базе данных имеет свой Id, который используется как первичный ключ.
+        public int BookId { get; set; } //Идентификатор книги, которая была выдана студенту. Это внешний ключ, который связывает запись Loan с конкретной книгой в таблице Books.
+        public Book Book { get; set; } //Навигационное свойство для связи с моделью Book. Позволяет получить информацию о книге, которая была выдана.
+        public int StudentId { get; set; } // Идентификатор студента, который взял книгу. Это внешний ключ, который связывает запись Loan с конкретным студентом в таблице Students.
+        public Student Student { get; set; } // Навигационное свойство для связи с моделью Student. Позволяет получить информацию о студенте, который взял книгу.
+        public DateTime LoanDate { get; set; } = DateTime.Now; //По умолчанию устанавливается в текущее время (DateTime.Now), когда создаётся новая запись.
+        public DateTime? ReturnDate { get; set; } //Дата возврата книги. Может быть null, если книга ещё не возвращена. Как только студент вернёт книгу, это поле заполняется соответствующей датой.
     }
 }
