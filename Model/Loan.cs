5a00134ce590ef46d397939b38217de6fe0312e0
Артур Стариков <Gibyte.mail@gmail.com>
2025-02-04 17:31:51 +0300

    Loan

---
+++ Model/Loan.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentLibrary.Model
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public DateTime LoanDate { get; set; } = DateTime.Now;
+        public DateTime? ReturnDate { get; set; }
+    }
+}
