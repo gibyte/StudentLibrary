0e8c2b15cb5041f7dd34cb1de890a6fcc852866b
Артур Стариков <Gibyte.mail@gmail.com>
2025-02-04 17:40:12 +0300

    required or ?

--- Model/Book.cs
+++ Model/Book.cs
@@ -3,8 +3,8 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
         public int Year { get; set; }
         public bool IsAvailable { get; set; } = true;
     }
