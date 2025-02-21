92cc8cb7fb021ad2fb2ccc17b6ca692caeedfc94
Артур Стариков <Gibyte.mail@gmail.com>
2025-02-21 18:16:18 +0300

    add page

---
+++ Migrations/20250221151510_UpStudent.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Comments",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Course",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Faculty",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Faculty",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Students");
+        }
+    }
+}
---
+++ Pages/Students.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentLibrary.Data;
+using StudentLibrary.Model;
+
+namespace StudentLibrary.Pages
+{
+    public class StudentsModel(ApplicationDbContext context) : PageModel
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        public List<Student> Students { get; set; } = [];
+
+        public void OnGet()
+        {
+            Students = _context.Students.ToList();
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return RedirectToPage();
+        }
+
+    }
+}
