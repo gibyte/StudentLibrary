0a4095d86852d482e36c180aa5d82098ca6bc1c0
Артур Стариков <Gibyte.mail@gmail.com>
2025-02-21 18:35:10 +0300

    student up del

---
+++ Pages/Student.cshtml.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentLibrary.Data;
+using StudentLibrary.Model;
+
+namespace StudentLibrary.Pages
+{
+    public class StudentModel(ApplicationDbContext context) : PageModel
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        [BindProperty]
+        public required Student Student { get; set; }
+
+        public void OnGet(int id)
+        {
+            if (id > 0)
+            {
+                Student = _context.Students.FirstOrDefault(b => b.Id == id);
+            }
+            else
+            {
+                Student = new Student() { Name = "Новый" };
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (Student.Id == 0)
+            {
+                _context.Students.Add(Student);
+            }
+            else
+            {
+                _context.Students.Update(Student);
+            }
+            _context.SaveChanges();
+            return RedirectToPage("Students");
+        }
+    }
+}
