c404ba3aa2dd10db279487ddf34318e734103996
Артур Стариков <Gibyte.mail@gmail.com>
2025-04-12 08:26:27 +0300

    Редактирование профиля

--- Pages/Account/Profile.cshtml.cs
+++ Pages/Account/Profile.cshtml.cs
@@ -1,17 +1,51 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentLibrary.Data;
+using StudentLibrary.Model.AuthApp;
 
 namespace StudentLibrary.Pages.Account
 {
     [Authorize]
     public class ProfileModel : PageModel
     {
-        public string UserEmail { get; set; }
+        private readonly ApplicationDbContext _context;
 
-        public void OnGet()
+        public ProfileModel(ApplicationDbContext context)
         {
-            UserEmail = User.Identity?.Name ?? "Неизвестный пользователь";
+            _context = context;
+        }
+
+        [BindProperty]
+        public AuthUser CurrentUser { get; set; }
+
+        public bool IsAdmin => User.IsInRole("Admin");
+
+        public IActionResult OnGet()
+        {
+            var email = User.Identity?.Name;
+            if (email == null) return RedirectToPage("/Account/Login");
+
+            CurrentUser = _context.AuthUsers.FirstOrDefault(u => u.Email == email);
+            if (CurrentUser == null) return NotFound();
+
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var user = _context.AuthUsers.FirstOrDefault(u => u.Id == CurrentUser.Id);
+            if (user == null) return NotFound();
+
+            if (User.IsInRole("Admin"))
+            {
+                user.Role = CurrentUser.Role;
+                _context.SaveChanges();
+            }
+
+            return RedirectToPage();
         }
     }
 }
---
+++ Pages/Account/Profiles.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentLibrary.Data;
+using StudentLibrary.Model.AuthApp;
+
+namespace StudentLibrary.Pages.Account
+{
+    //[Authorize(Roles = "Admin")]
+    public class ProfilesModel : PageModel
+    {
+        
+        private readonly ApplicationDbContext _context;
+        public ProfilesModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<AuthUser> Users { get; set; } = new();
+
+        public void OnGet()
+        {
+            Users = _context.AuthUsers.ToList();
+        }
+        
+    }
+}
