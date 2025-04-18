6434c1a701511a14481a32bc8d9691b60c5e1a44
Артур Стариков <Gibyte.mail@gmail.com>
2025-04-18 15:55:29 +0300

    контроллеры

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudentLibrary.Data;
+using StudentLibrary.Model;
+
+namespace StudentLibrary.Controllers
+{
+    [Authorize]
+    [Route("controller/students")] // Этот маршрут будет использоваться для всех действий
+    public class StudentsController(ApplicationDbContext context) : Controller
+    {
+        private readonly ApplicationDbContext _context = context;
+
+        // GET: controller/students
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            var students = _context.Students.ToList();
+            return View(students);
+        }
+
+        // POST: controller/students/delete/{id}
+        [HttpPost("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: controller/students/edit/{id}
+        [HttpGet("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            if (id.HasValue && id > 0)
+            {
+                var student = _context.Students.FirstOrDefault(s => s.Id == id);
+                if (student == null) return NotFound();
+                return View(student);
+            }
+            return View(new Student() { Name = "Новый студент" });
+        }
+
+        // POST: controller/students/edit
+        [HttpPost("edit")]
+        public IActionResult Edit(Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(student);
+            }
+
+            if (student.Id == 0)
+            {
+                _context.Students.Add(student);
+            }
+            else
+            {
+                _context.Students.Update(student);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using StudentLibrary.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddHttpContextAccessor(); // авторизация
+
+builder.Services.AddControllersWithViews();//
 
 // Настройка подключения к базе данных
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
