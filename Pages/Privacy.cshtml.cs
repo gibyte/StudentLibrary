ba13877b1215cf5b8f5f282cb68dbcb6964a1dfa
Артур Стариков <Gibyte.mail@gmail.com>
2025-04-26 09:32:10 +0300

    test

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.SignalR;
-using Microsoft.AspNetCore.SignalR.Client;
 using StudentLibrary.Hubs;
 
 namespace StudentLibrary.Pages
